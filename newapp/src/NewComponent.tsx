cf4d642f14d8fe47d2b272ecf6c094d40056fa21
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-08 22:49:08 +0300

    08112022

--- newapp/src/NewComponent.tsx
+++ newapp/src/NewComponent.tsx
@@ -10,38 +10,21 @@ const topCars = [
     {manufacturer: 'Mercedes', model: 'e63s'},
     {manufacturer: 'Audi', model: 'rs6'}
 ]
-//     <table>
-//     <tr>
-//     <th>Month</th>
-// <th>Savings</th>
-// </tr>
-// <tr>
-//     <td>January</td>
-//     <td>$100</td>
-// </tr>
-// <tr>
-//     <td>February</td>
-//     <td>$80</td>
-// </tr>
-// </table>
 
 export const CarComponent = () => {
     return (
         <ul>
             {topCars.map((objectFromCarArray: carType, index) => {
                 return (
-                    // <li key={objectFromStudentArray.id}>
-                    //     <span>
-                    //        {objectFromStudentArray.name}
-                    //     </span>
-                    //     <span> age: {objectFromStudentArray.age} </span>
-                    // </li>
-                    <li key={index + 1}>
-                        <span>
+                    <div> {index + 1}
+                        <li>
+
+                        <span key={index + 1}>
                            {objectFromCarArray.manufacturer}
                         </span>
-                        <span> age: {objectFromCarArray.model} </span>
-                    </li>
+                            <span> age: {objectFromCarArray.model} </span>
+                        </li>
+                    </div>
                 )
 
             })}
