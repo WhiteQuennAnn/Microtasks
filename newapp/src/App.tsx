1c6e0c0ca4ec413e7a12997975228c701cfb31e7
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-08 22:29:04 +0300

    08112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -6,12 +6,26 @@ import { Body } from './site/Body';
 import {Footer} from "./site/Footer";
 import {NewComponent} from "./NewComponent";
 
+const students = [
+    {id: 1, name: "James", age: 8},
+    {id: 2, name: "Robert", age: 18},
+    {id: 3, name: "John", age: 28},
+    {id: 4, name: "Michael", age: 38},
+    {id: 5, name: "William", age: 48},
+    {id: 6, name: "David", age: 58},
+    {id: 7, name: "Richard", age: 68},
+    {id: 8, name: "Joseph", age: 78},
+    {id: 9, name: "Thomas", age: 88},
+    {id: 10, name: "Charles", age: 98},
+    {id: 11, name: "Christopher", age: 100},
+]
 function App() {
     return (
         <>
             <Header title={"New Body"}/>
             <Body titleForBody={"New Body"}/>
             <Footer titleForFooter={"New Body"}/>
+            <NewComponent students={students}/>
 
         </>
     );
--- newapp/src/NewComponent.tsx
+++ newapp/src/NewComponent.tsx
@@ -6,19 +6,7 @@ type StudentType = {
     age: number
 }
 
-const students = [
-    {id: 1, name: "James", age: 8},
-    {id: 2, name: "Robert", age: 18},
-    {id: 3, name: "John", age: 28},
-    {id: 4, name: "Michael", age: 38},
-    {id: 5, name: "William", age: 48},
-    {id: 6, name: "David", age: 58},
-    {id: 7, name: "Richard", age: 68},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 9, name: "Thomas", age: 88},
-    {id: 10, name: "Charles", age: 98},
-    {id: 11, name: "Christopher", age: 100},
-]
+
 
 type NewComponentType = {
     students: Array<StudentType>
