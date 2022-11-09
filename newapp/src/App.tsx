7a9563d7fcede8e483f9133889208e058fa1a9b0
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 21:03:51 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -15,11 +15,12 @@ function App() {
     //
     const foo1 = () => {
        //ничего не передавать, но в консоли должно показаться 100200
+        console.log (100200)
     }
 
-    const foo2 = () => {
+    const foo2 = (numb:number) => {
         //передать цифру 100200
-        console.log (100200)
+        console.log (numb)
     }
     return (
         <div className="App">
@@ -30,7 +31,7 @@ function App() {
             {/*</button> <button onClick={(event: MouseEvent<HTMLButtonElement) => onClickHandler(name: "Juy")}> MyYouTubeChannel-3*/}
             {/*</button>*/}
             <button onClick={foo1}>1</button>
-            <button onClick={foo2}>2</button>
+            <button onClick={()=>foo2(100200)}>2</button>
         </div>
     );
 }
