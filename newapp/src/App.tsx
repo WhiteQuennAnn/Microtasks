cebc6fdd7430be294480c0824ffb968260471b02
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 20:06:19 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -3,9 +3,21 @@ import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
+    const myFirstSubscriber =() => {
+        console.log("Hello, i am Tanya")
+    }
+ const mySecondSubscriber =() => {
+        console.log("Hello, i am POl")
+    }
+
+
+
+
     return (
         <div className={"App"}>
-        <button> MyYouTubeChannel-1</button>
+        {/*<button onClick={(event)=>{console.log("Hello")}}> MyYouTubeChannel-1</button>*/}
+        <button onClick={myFirstSubscriber}> MyYouTubeChannel-1</button>
+        <button onClick={mySecondSubscriber}> MyYouTubeChannel-2</button>
 
         </div>
     );
