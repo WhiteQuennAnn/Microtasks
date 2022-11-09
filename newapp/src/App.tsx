d5790193be223d5443b007d6898989670e7c6c93
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-10 00:31:07 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import {Button} from "./site/Components/Button";
 import './App.css';
 
+type FilterType = "all" | "RUBLS" | "Dollars"
+
 function App() {
 
     const [money, setMoney] = useState([
@@ -14,19 +16,20 @@ function App() {
         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
     ])
+    const[Filter, setFilter] = useState<FilterType>("all")
 
     let currentMoney = money;
-    const onclickFilterHandler = (nameButton: string) => {
-
-        if (nameButton === 'RUBLS') {
-            currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS');
-        }
-        if (nameButton === 'RUBLS') {
-            currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars');
-        }
+    if (Filter === 'RUBLS') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS');
+    }
+    if (Filter === 'Dollars') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars');
     }
 
 
+const onclickFilterHandler = (nameButton: FilterType) => {
+    setFilter(nameButton);
+}
     return (
         <>
             <ul>
@@ -42,11 +45,11 @@ function App() {
             </ul>
             <div style={{marginLeft: "35px"}}>
                 <button onClick={() => onclickFilterHandler("all")}> all</button>
-                <button onClick={() => onclickFilterHandler("ruble")}> ruble</button>
-                <button onClick={() => onclickFilterHandler("dollar")}> dollar</button>
+                <button onClick={() => onclickFilterHandler("RUBLS")}> ruble</button>
+                <button onClick={() => onclickFilterHandler("Dollars")}> dollar</button>
             </div>
         </>
-    );
-}
+    )
 
+}
 export default App;
