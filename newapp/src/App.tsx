814a78989c687580ad3f47f2ad11a7ccc3710112
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 23:42:08 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -4,42 +4,38 @@ import './App.css';
 
 function App() {
 
-    // let a = 1
-    let[a, setA]=useState(1)
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
 
-    const onclickHandler=() => {
-       setA(++a)
-        console.log(a)
-    }
-    const onclickHandler1=() => {
-        setA(0)
-        console.log(a)
-    }
-
-    // const Button1Foo =(suscriber: string, age:number)=> {
-    //     console.log(suscriber,age)
-    //
-    // }
-    // const Button2Foo =(suscriber: string,  age:number)=> {
-    //     console.log(suscriber, age)
-    // }
-    // const Button3Foo =()=> {
-    //     console.log("I am stupid button")
-    // }
+    let currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
 
     return (
-
-        <div className={"App"}>
-<h1> {a}</h1>
-            <button onClick={onclickHandler}> number</button>
-            <button onClick={onclickHandler1}> 0 </button>
-
-            {/*<Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("i am Anna", 23)}/>*/}
-            {/*<Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Den", 34)}/>*/}
-            {/*<Button name={"MyYouTubeChannel-3"} callBack={Button3Foo}/>*/}
-
-
-        </div>
+<>
+        <ul>
+            {currentMoney.map((objFromMoneyArr, index) => {
+                return (
+                    <li key={index}>
+                        <span> {objFromMoneyArr.banknots}</span>
+                        <span> {objFromMoneyArr.value}</span>
+                        <span> {objFromMoneyArr.number}</span>
+                    </li>
+                )
+            })}
+        </ul>
+    <div style={{marginLeft: "35px"}}>
+        <button> all</button>
+        <button> ruble</button>
+        <button> dollar</button>
+    </div>
+</>
     );
 }
 
