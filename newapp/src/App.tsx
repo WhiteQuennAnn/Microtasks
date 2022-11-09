7e563ef9282ba48abb651d6198bdcdef3f5f2fe3
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 22:24:43 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from "./site/Components/Button";
 import './App.css';
 
 function App() {
 
-    let a = 1
+    // let a = 1
+    let[a, setA]=useState(1)
+
+    const onclickHandler=() => {
+       setA(++a)
+        console.log(a)
+    }
     // const Button1Foo =(suscriber: string, age:number)=> {
     //     console.log(suscriber,age)
     //
@@ -20,6 +26,8 @@ function App() {
 
         <div className={"App"}>
 <h1> {a}</h1>
+            <button onClick={onclickHandler}> number</button>
+
             {/*<Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("i am Anna", 23)}/>*/}
             {/*<Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Den", 34)}/>*/}
             {/*<Button name={"MyYouTubeChannel-3"} callBack={Button3Foo}/>*/}
