ab811f372cddf778d5c30f79e0efcdb20563de83
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 21:50:33 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
     const Button1Foo =()=> {
-
+        console.log("i am Anna")
     }
     const Button2Foo =()=> {
-
+        console.log("i am Cot")
     }
 
     return (
@@ -16,7 +16,7 @@ function App() {
 
             <Button name={"MyYouTubeChannel-1"} callBack={Button1Foo}/>
             <Button name={"MyYouTubeChannel-2"} callBack={Button2Foo}/>
-            <Button name={"Delete"}/>
+
 
         </div>
     );
