3373bcfdd204c184f224c04785364e11d820a639
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 22:11:56 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -3,24 +3,26 @@ import {Button} from "./site/Components/Button";
 import './App.css';
 
 function App() {
-    const Button1Foo =(suscriber: string, age:number)=> {
-        console.log(suscriber,age)
 
-    }
-    const Button2Foo =(suscriber: string,  age:number)=> {
-        console.log(suscriber, age)
-    }
-    const Button3Foo =()=> {
-        console.log("I am stupid button")
-    }
+    let a = 1
+    // const Button1Foo =(suscriber: string, age:number)=> {
+    //     console.log(suscriber,age)
+    //
+    // }
+    // const Button2Foo =(suscriber: string,  age:number)=> {
+    //     console.log(suscriber, age)
+    // }
+    // const Button3Foo =()=> {
+    //     console.log("I am stupid button")
+    // }
 
     return (
 
         <div className={"App"}>
-
-            <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("i am Anna", 23)}/>
-            <Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Den", 34)}/>
-            <Button name={"MyYouTubeChannel-3"} callBack={Button3Foo}/>
+<h1> {a}</h1>
+            {/*<Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("i am Anna", 23)}/>*/}
+            {/*<Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Den", 34)}/>*/}
+            {/*<Button name={"MyYouTubeChannel-3"} callBack={Button3Foo}/>*/}
 
 
         </div>
