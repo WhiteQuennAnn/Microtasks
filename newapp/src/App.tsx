fa683ba801aa2e76c415b99b6c7964e7302c9b5e
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 20:38:46 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -3,12 +3,15 @@ import React, {MouseEvent} from 'react';
 import './App.css';
 
 function App() {
-    const myFirstSubscriber =(event: MouseEvent<HTMLButtonElement) => {
-        console.log("Hello, i am Tanya")
-    }
- const mySecondSubscriber =(event: MouseEvent<HTMLButtonElement) => {
-        console.log("Hello, i am POl")
-    }
+ //    const myFirstSubscriber =(event: MouseEvent<HTMLButtonElement) => {
+ //        console.log("Hello, i am Tanya")
+ //    }
+ // const mySecondSubscriber =(event: MouseEvent<HTMLButtonElement) => {
+ //        console.log("Hello, i am POl")
+ //    }
+ const onClickHandler=(name: string)=> {
+     console.log(name)
+ }
 
 
 
@@ -16,9 +19,8 @@ function App() {
     return (
         <div className={"App"}>
         {/*<button onClick={(event)=>{console.log("Hello")}}> MyYouTubeChannel-1</button>*/}
-        <button onClick={myFirstSubscriber}> MyYouTubeChannel-1</button>
-        <button onClick={mySecondSubscriber}> MyYouTubeChannel-2</button>
-
+        <button onClick={(event: MouseEvent<HTMLButtonElement) =>onClickHandler(name: "Lila")}> MyYouTubeChannel-1</button>
+        <button onClick={ (event: MouseEvent<HTMLButtonElement) =>onClickHandler(name: "Juy")}> MyYouTubeChannel-2</button>
         </div>
     );
 }
