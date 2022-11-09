eb5052d86420510399fe209b39162bce64cbb5b4
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-09 21:56:14 +0300

    09112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -3,19 +3,20 @@ import {Button} from "./site/Components/Button";
 import './App.css';
 
 function App() {
-    const Button1Foo =(suscriber: string)=> {
-        console.log(suscriber: string)
+    const Button1Foo =(suscriber: string, age:number)=> {
+        console.log(suscriber,age)
+
     }
-    const Button2Foo =(suscriber: string)=> {
-        console.log(suscriber: string)
+    const Button2Foo =(suscriber: string,  age:number)=> {
+        console.log(suscriber, age)
     }
 
     return (
 
         <div className={"App"}>
 
-            <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("i am Anna")}/>
-            <Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Den")}/>
+            <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("i am Anna", 23)}/>
+            <Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I am Den", 34)}/>
 
 
         </div>
