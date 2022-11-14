840923a157a0387dff25a05b6cbf08f0abfec65e
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-14 20:20:13 +0300

    10112022

--- newapp/src/App.tsx
+++ newapp/src/App.tsx
@@ -1,60 +1,23 @@
-import React, {useState} from 'react';
-import {Button} from "./site/Components/Button";
-import './App.css';
-import Dz from "./site/Components/DZ";
-
-
-export type FilterType = "all" | "RUBLS" | "Dollars"
-
 function App() {
-
-    const [money, setMoney] = useState([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ])
-    const [Filter, setFilter] = useState<FilterType>("all")
-
-    let currentMoney = money;
-    if (Filter === 'RUBLS') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS');
-    }
-    if (Filter === 'Dollars') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars');
-    }
-
-
-    const onclickFilterHandler = (nameButton: FilterType) => {
-        setFilter(nameButton);
-    }
-    return (
-        <Dz  currentMoney={ currentMoney} onclickFilterHandler={onclickFilterHandler}/>
-
-        // <>
-        //     <ul>
-        //         {currentMoney.map((objFromMoneyArr, index) => {
-        //             return (
-        //                 <li key={index}>
-        //                     <span> {objFromMoneyArr.banknots}</span>
-        //                     <span> {objFromMoneyArr.value}</span>
-        //                     <span> {objFromMoneyArr.number}</span>
-        //                 </li>
-        //             )
-        //         })}
-        //     </ul>
-        //     <div style={{marginLeft: "35px"}}>
-        //         <button onClick={() => onclickFilterHandler("all")}> all</button>
-        //         <button onClick={() => onclickFilterHandler("RUBLS")}> ruble</button>
-        //         <button onClick={() => onclickFilterHandler("Dollars")}> dollar</button>
-        //     </div>
-        // </>
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
     )
-
+    return (
+        <div className="App">
+            <div>
+                <input />
+                <button>+</button>
+            </div>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    );
 }
-
-export default App;
